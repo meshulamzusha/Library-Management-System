d2538fc5b0df6d565ed45da9bda6ac00374245d9
Your Name <you@example.com>
2025-11-11 11:15:16 +0200

    definition of str method in class Book

--- classes/book.py
+++ classes/book.py
@@ -4,3 +4,6 @@ class Book:
         self.author = author
         self.isbn = isbn
         self.is_available = is_available
+
+    def __str__(self):
+        return f"title: {self.title}, author: {self.author}, isbn: {self.isbn}, available: {self.is_available}"
