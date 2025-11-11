3b44d14ad3153b08bd04634e8c5990515624540a
Your Name <you@example.com>
2025-11-11 11:13:45 +0200

    definition of constractor in class Book

--- classes/book.py
+++ classes/book.py
@@ -1,2 +1,6 @@
 class Book:
-    pass
+    def __init__(self, title, author, isbn, is_available=True):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.is_available = is_available
