522be6fe18d8ec789e710c9232ad70aeb50f00d4
Aviel Oiknine <avieloikpro@gmail.com>
2025-11-11 11:14:37 +0200

    methods : borrow_book and return_book

--- classes/user.py
+++ classes/user.py
@@ -5,4 +5,9 @@ class User:
         self.name = name
         self.borrowed_books = []
 
-    
+    def borrow_book(self, book):
+        self.borrowed_books.append(book.isbn)
+
+    def return_book(self, book):
+        if book.isbn in self.borrowed_books:
+            self.borrowed_books.remove(book.isbn)
